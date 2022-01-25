8832fb36636459ee8246be633f4910edcc904670
ciandafydd16@gmail.com <ciandafydd16@gmail.com>
2022-01-25 15:20:19 +0000

    Greetings

--- week2.py
+++ week2.py
@@ -1,2 +1,8 @@
-names = ["Cian", "Jack", "Leah", "Elliot", "Cai", "Eben"]
-print(sorted(names))
+names = "Cian", "Jack", "Leah", "Elliot", "Cai", "Eben"
+message = "welcome."
+print(names[0],message)
+print(names[2],message)
+print(names[1],message)
+print(names[4],message)
+print(names[5],message)
+print(names[3],message)
